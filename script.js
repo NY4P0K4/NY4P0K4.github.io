9682ca99e5f93b02cb5884ee3959b20df9e439e5
Lucas V <lucas_debastiani@estudante.sesisenai.org.br>
2026-04-29 07:44:57 -0300

    removido S de naturezas

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ const nats = {
   'matematica' : [['https://docs.google.com/document/d/1dzi8q8Jtgf9WkELcb_ktnXoZdaPjRA4t0UIUcXhQVKU/edit?usp=sharing', 'Quebrando a Banca', ['C5'], ['H31', 'H32']], ['https://canva.link/99qkmkowvxmjlld', 'Jogo: Kassino', ['C5'], ['H30', 'H31']]],
   'linguagens' : [['https://canva.link/0ob1jdygsqsw6u0', 'A Paixão, Segundo G.H.', ['?'], ['H4', 'H22']], ['https://wordwall.net/pt/resource/108331158/2ª-geração-do-romantismo-brasileiro', 'Game Literário', ['?'], ['H15']]],
   'humanas' : [['https://canva.link/3othh3sfqfxx90z', 'Apresentação Geopolítica', ['C1'], ['H1', 'H2', 'H3', 'H4', 'H5']], ['https://www.canva.com/design/DAHDv1aRlMk/NehFtNc3D8n7L4rDWQKsNQ', 'Neocolonialismo: Raj Britânico', ['C2'], ['H8', 'H10', 'H12']], ['https://canva.link/cmu56393aa0gbc9', 'PRAVDA: A Grande Guerra', ['C3'], ['H15', 'H16', 'H20']]],
-  'naturezas' : [['av1.jpeg', 'Meme Evolucionista', ['C3'], ['H15', 'H18']], ['https://canva.link/aqp3r6b0yluvr1s', 'Guerras e Conflitos', ['C1', 'C2'], ['H1', 'H9', 'H11']], ['https://docs.google.com/document/d/1HzSLVOPeMkOP1Y9pMdw6wIFHOChm4IY-gay8iseYlQE/edit?usp=sharing', 'Disputa Elétrica', ['C1', 'C2'], ['H1', 'H7', 'H9', 'H11', 'H12']]],
+  'natureza' : [['av1.jpeg', 'Meme Evolucionista', ['C3'], ['H15', 'H18']], ['https://canva.link/aqp3r6b0yluvr1s', 'Guerras e Conflitos', ['C1', 'C2'], ['H1', 'H9', 'H11']], ['https://docs.google.com/document/d/1HzSLVOPeMkOP1Y9pMdw6wIFHOChm4IY-gay8iseYlQE/edit?usp=sharing', 'Disputa Elétrica', ['C1', 'C2'], ['H1', 'H7', 'H9', 'H11', 'H12']]],
   'tecnico' : [['https://canva.link/0u0xn19zejrxmo0', 'Canva: Grand Prix', ['?'], ['?']]]
 };
 
